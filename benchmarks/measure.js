a3c6740456e303c247366c67f34218bd926059d8
ehmicky <ehmicky@gmail.com>
2019-05-28 10:00:00 +0200

    Improve benchmark

--- benchmarks/measure.js
+++ benchmarks/measure.js
@@ -1,13 +1,10 @@
 import { performance } from 'perf_hooks'
 
-import { promiseThen } from './promise.js'
-
-// Measure how long a sync or async function take to execute
+// Measure how long a function takes to execute
 export const measure = function(func) {
   const start = performance.now()
-  return promiseThen(func(), () => {
-    const end = performance.now()
-    const duration = end - start
-    return duration
-  })
+  func()
+  const end = performance.now()
+  const duration = end - start
+  return duration
 }
