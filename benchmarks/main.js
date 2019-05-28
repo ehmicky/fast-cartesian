fca18f61afd4930ba563e34582a23d6496004c9f
ehmicky <ehmicky@gmail.com>
2019-05-28 10:00:00 +0200

    Add benchmarks

--- benchmarks/main.js
+++ benchmarks/main.js
@@ -1,8 +1,39 @@
+import cartesianProduct from 'cartesian-product'
+import cartesian from 'cartesian'
+import lodash from 'lodash'
+// eslint-disable-next-line import/no-unassigned-import
+import 'lodash.product'
+
 import fastCartesian from '../src/main.js'
 
 import { benchmark } from './benchmark.js'
 import { variants } from './variants.js'
 
-const testCartesian = { title: 'test-cartesian', main: fastCartesian, variants }
+const testFastCartesian = {
+  title: 'fast-cartesian',
+  main: (...args) => fastCartesian(...args),
+  variants,
+}
 
-benchmark({ testCartesian }, { repeat: 1e2 })
+const testCartesianProduct = {
+  title: 'cartesian-product',
+  main: (...args) => cartesianProduct(args),
+  variants,
+}
+
+const testCartesian = {
+  title: 'cartesian',
+  main: (...args) => cartesian(args),
+  variants,
+}
+
+const testLodashProduct = {
+  title: 'lodash.product',
+  main: (...args) => lodash.product(...args),
+  variants,
+}
+
+benchmark(
+  { testFastCartesian, testCartesianProduct, testCartesian, testLodashProduct },
+  { repeat: 1e2 },
+)
