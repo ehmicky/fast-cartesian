9431b64894de9a139c7fde58ca5cc5503376e508
ehmicky <ehmicky@gmail.com>
2019-05-28 10:00:00 +0200

    Improve benchmark

--- benchmarks/results.js
+++ benchmarks/results.js
@@ -4,18 +4,24 @@ import { getStats } from './stats.js'
 
 export const getResults = function({ tasks, options, options: { repeat } }) {
   const loop = getArray(repeat)
-  return Object.entries(tasks).flatMap(([id, { title = id, main, variants }]) =>
-    getResult({ title, main, variants, loop, options }),
+  return Object.entries(tasks).flatMap(([id, task]) =>
+    getResult({ id, task, loop, options }),
   )
 }
 
-const getResult = function({ title, main, variants, loop, options }) {
+const getResult = function({
+  id,
+  task,
+  task: { title = id, main, variants },
+  loop,
+  options,
+}) {
   if (variants === undefined) {
     return getArgResult({ title, main, loop, options })
   }
 
   return Object.entries(variants).map(([variantTitle, args]) =>
-    getArgResult({ title, main, args, variantTitle, loop, options }),
+    getArgResult({ title, main, args, variantTitle, loop, task, options }),
   )
 }
 
@@ -25,6 +31,7 @@ const getArgResult = function({
   args,
   variantTitle,
   loop,
+  task,
   options,
 }) {
   const titleA =
@@ -33,5 +40,5 @@ const getArgResult = function({
   const mainA = args === undefined ? main : main.bind(null, ...args)
   const durations = loop.map(() => measure(mainA))
   const duration = getStats(durations)
-  return { title: titleA, args, duration, options }
+  return { title: titleA, task, args, duration, options }
 }
