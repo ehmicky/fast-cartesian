5da051304ded985e60a52844c542128889afbd89
ehmicky <ehmicky@gmail.com>
2019-07-08 10:00:00 +0200

    Update tests

--- test/main.js
+++ test/main.js
@@ -3,36 +3,38 @@ import prettyFormat from 'pretty-format'
 
 import { cartesianArray, cartesianIterate } from '../src/main.js'
 
-const ARGS = [
-  [],
-  [[]],
-  [[], []],
-  [undefined],
-  [null],
-  [[], true],
-  [[0]],
-  [[0], [1]],
-  [[0, 1]],
-  [[0, 1], [2]],
-  [[0, 1], [2, 3]],
-  // eslint-disable-next-line no-magic-numbers
-  [[0, 1, 2], [3, 4]],
-  [[[0]]],
-  [[0, undefined, 1]],
-]
-
 const METHODS = [cartesianArray, cartesianIterate]
 
 METHODS.forEach(cartesian => {
+  const ARGS = [
+    [],
+    [[]],
+    [[], []],
+    [[0]],
+    [[0], [1]],
+    [[0, 1]],
+    [[0, 1], [2]],
+    [[0, 1], [2, 3]],
+    // eslint-disable-next-line no-magic-numbers
+    [[0, 1, 2], [3, 4]],
+    [[[0]]],
+    [[0, undefined, 1]],
+  ]
   ARGS.forEach(args => {
     const title = prettyFormat(args, { min: true })
     // eslint-disable-next-line max-nested-callbacks
     test(`${cartesian.name} ${title}`, t => {
-      try {
-        t.snapshot([...cartesian(...args)])
-      } catch (error) {
-        t.snapshot(error)
-      }
+      t.snapshot([...cartesian(...args)])
+    })
+  })
+
+  const INVALID_ARGS = [[undefined], [null], [[], true]]
+  INVALID_ARGS.forEach(args => {
+    const title = prettyFormat(args, { min: true })
+    // eslint-disable-next-line max-nested-callbacks
+    test(`${cartesian.name} ${title}`, t => {
+      // eslint-disable-next-line max-nested-callbacks
+      t.throws(() => [...cartesian(...args)])
     })
   })
 })
