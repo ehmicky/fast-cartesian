eaa3ec76f1d2821d76100a3b7faf859ed371c151
ehmicky <ehmicky@gmail.com>
2019-08-11 17:18:57 +0200

    Refactor benchmarks

--- benchmarks/main.js
+++ benchmarks/main.js
@@ -7,10 +7,6 @@ import FastCartesianProduct from 'fast-cartesian-product'
 
 import { cartesianArray } from '../src/main.js'
 
-import { getVariations } from './variations.js'
-
-export const variations = getVariations(5)
-
 export const fastCartesian = {
   title: 'fast-cartesian',
   main: args => cartesianArray(...args),
@@ -35,3 +31,31 @@ export const lodashProduct = {
   title: 'lodash.product',
   main: args => lodash.product(...args),
 }
+
+// Retrieve several arguments for cartesian products:
+//  - the return value length is always the same
+//  - the number of dimensions and size of each array differ
+// For example `length: 3` returns:
+///  [[0, 1, 2, 3]] (1 dimension)
+//   [[0, 1], [0, 1]] (2 dimensions)
+//   [[0], [0], [0], [0]] (4 dimensions)
+const getVariations = function(length) {
+  return Array.from({ length }, (value, index) => getVariation(index, length))
+}
+
+const getVariation = function(index, length) {
+  const dimensions = 2 ** index
+  const unitLength = 2 ** (2 ** (length - index - 1))
+  const unit = Array.from({ length: unitLength }, getIndex)
+  const value = Array.from({ length: dimensions }, () => unit)
+  const id = String(index + 1)
+  const title =
+    dimensions === 1 ? `${dimensions} array` : `${dimensions} arrays`
+  return { id, title, value }
+}
+
+const getIndex = function(value, index) {
+  return index
+}
+
+export const variations = getVariations(5)
