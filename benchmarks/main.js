5aa723eeb9d7722eef01b19cbd19156f4f0ab674
ehmicky <ehmicky@gmail.com>
2019-08-18 23:03:43 +0200

    Add power-cartesian-product to benchmarks

--- benchmarks/main.js
+++ benchmarks/main.js
@@ -3,7 +3,7 @@ import cartesianFunc from 'cartesian'
 import lodash from 'lodash'
 // eslint-disable-next-line import/no-unassigned-import
 import 'lodash.product'
-import FastCartesianProduct from 'fast-cartesian-product'
+import PowerCartesianProduct from 'power-cartesian-product'
 
 import { cartesianArray } from '../src/main.js'
 
@@ -17,9 +17,9 @@ export const cartesianProduct = {
   main: args => cartesianProductFunc(args),
 }
 
-export const fastCartesianProduct = {
-  title: 'fast-cartesian-product',
-  main: args => [...new FastCartesianProduct(args)],
+export const powerCartesianProduct = {
+  title: 'power-cartesian-product',
+  main: args => [...new PowerCartesianProduct(args)],
 }
 
 export const cartesian = {
