f6f3d980664d788d8c76b9bf550da14befcbad86
ehmicky <ehmicky@gmail.com>
2021-06-20 23:22:31 +0200

    Fix linting

--- examples/main.js
+++ examples/main.js
@@ -5,7 +5,6 @@
 // An online demo is also available at:
 //   https://repl.it/@ehmicky/fast-cartesian
 
-// eslint-disable-next-line node/no-missing-import
 import fastCartesian from 'fast-cartesian'
 
 console.log(
