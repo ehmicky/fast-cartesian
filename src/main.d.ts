6613ab2fa7aa0299b087e14cf9eb25c72f4612e7
Paul Heidenreich <paul@paul-h.de>
2021-12-03 17:15:15 +0100

    Update typescript definition to be more precise
    Now any number of factors is supported. Each factor can have is own type

--- src/main.d.ts
+++ src/main.d.ts
@@ -1,2 +1,5 @@
-declare const fastCartesian: <T>(arrays: T[][]) => T[][]
+declare const fastCartesian: <TFactors extends any[][]>(factors: [...TFactors]) => {
+  [TFactor in keyof TFactors]: TFactors[TFactor] extends Array<infer TUnArrayed> ? TUnArrayed : never
+}[]
+
 export default fastCartesian
