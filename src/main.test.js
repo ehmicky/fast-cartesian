4963c732fe5a9b3a66499fdd208176b3faabd704
ehmicky <ehmicky@gmail.com>
2022-12-04 23:39:11 +0100

    Refactor tests

--- src/main.test.js
+++ src/main.test.js
@@ -1,106 +1,54 @@
 import test from 'ava'
 import fastCartesian from 'fast-cartesian'
-import { format } from 'pretty-format'
+import { each } from 'test-each'
 
-const getTitle = function (args) {
-  return format(args, { min: true })
-}
-
-const ARGS = [
-  { input: [], output: [] },
-  { input: [[]], output: [] },
-  { input: [[], []], output: [] },
-  { input: [[0]], output: [[0]] },
-  { input: [[0], [1]], output: [[0, 1]] },
-  { input: [[0, 1]], output: [[0], [1]] },
-  {
-    input: [[0, 1], [2]],
-    output: [
-      [0, 2],
-      [1, 2],
-    ],
-  },
-  {
-    input: [
-      [0, 1],
-      [2, 3],
-    ],
-    output: [
-      [0, 2],
-      [0, 3],
-      [1, 2],
-      [1, 3],
-    ],
-  },
-  {
-    input: [
-      [0, 1, 2],
-      [3, 2],
-    ],
-    output: [
-      [0, 3],
-      [0, 2],
-      [1, 3],
-      [1, 2],
-      [2, 3],
-      [2, 2],
-    ],
-  },
-  { input: [[[0]]], output: [[[0]]] },
-  { input: [[0, undefined, 1]], output: [[0], [undefined], [1]] },
-]
-ARGS.forEach(({ input, output }) => {
-  test(`success | ${getTitle(input)}`, (t) => {
-    t.deepEqual(fastCartesian(input), output)
-  })
-})
-
-const INVALID_ARGS = [
-  true,
-  [undefined],
-  [null],
-  [[], true],
-  [() => true],
+each(
   [
-    function getObject() {
-      return {}
+    { input: [], output: [] },
+    { input: [[]], output: [] },
+    { input: [[], []], output: [] },
+    { input: [[0]], output: [[0]] },
+    { input: [[0], [1]], output: [[0, 1]] },
+    { input: [[0, 1]], output: [[0], [1]] },
+    {
+      input: [[0, 1], [2]],
+      output: [
+        [0, 2],
+        [1, 2],
+      ],
     },
-  ],
-  [
-    function getNull() {
-      return null
+    {
+      input: [
+        [0, 1],
+        [2, 3],
+      ],
+      output: [
+        [0, 2],
+        [0, 3],
+        [1, 2],
+        [1, 3],
+      ],
     },
-  ],
-  [
-    function getInvalidIterator() {
-      return { next: true }
+    {
+      input: [
+        [0, 1, 2],
+        [3, 2],
+      ],
+      output: [
+        [0, 3],
+        [0, 2],
+        [1, 3],
+        [1, 2],
+        [2, 3],
+        [2, 2],
+      ],
     },
+    { input: [[[0]]], output: [[[0]]] },
+    { input: [[0, undefined, 1]], output: [[0], [undefined], [1]] },
   ],
-]
-INVALID_ARGS.forEach((args) => {
-  test(`should throw | ${getTitle(args)}`, (t) => {
-    t.throws(fastCartesian.bind(undefined, args))
-  })
-})
-
-const COMBINATIONS = [
-  { length: 100, size: 1 },
-  { length: 32, size: 2 },
-  { length: 99, size: 1300 },
-]
-COMBINATIONS.forEach((combination) => {
-  test(`should throw on high number of combinations | ${getTitle(
-    combination,
-  )}`, (t) => {
-    const args = getBigArray(combination)
-    t.throws(fastCartesian.bind(undefined, args))
-  })
-})
-
-const getBigArray = function ({ length, size }) {
-  return Array.from({ length }, () => Array.from({ length: size }, getTrue))
-}
-
-const getTrue = function () {
-  return true
-}
+  ({ title }, { input, output }) => {
+    test(`success | ${title}`, (t) => {
+      t.deepEqual(fastCartesian(input), output)
+    })
+  },
+)
---
+++ src/validate.test.js
@@ -0,0 +1,55 @@
+import test from 'ava'
+import fastCartesian from 'fast-cartesian'
+import { each } from 'test-each'
+
+each(
+  [
+    true,
+    [undefined],
+    [null],
+    [[], true],
+    [() => true],
+    [
+      function getObject() {
+        return {}
+      },
+    ],
+    [
+      function getNull() {
+        return null
+      },
+    ],
+    [
+      function getInvalidIterator() {
+        return { next: true }
+      },
+    ],
+  ],
+  ({ title }, input) => {
+    test(`should throw | ${title}`, (t) => {
+      t.throws(fastCartesian.bind(undefined, input))
+    })
+  },
+)
+
+each(
+  [
+    { length: 100, size: 1 },
+    { length: 32, size: 2 },
+    { length: 99, size: 1300 },
+  ],
+  ({ title }, { length, size }) => {
+    test(`should throw on high number of combinations | ${title}`, (t) => {
+      const args = getBigArray(length, size)
+      t.throws(fastCartesian.bind(undefined, args))
+    })
+  },
+)
+
+const getBigArray = function (length, size) {
+  return Array.from({ length }, () => Array.from({ length: size }, getTrue))
+}
+
+const getTrue = function () {
+  return true
+}
